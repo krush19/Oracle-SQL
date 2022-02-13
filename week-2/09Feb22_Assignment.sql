0a1024c5108b3f0322ec04dad014d3ea7aabbf16
krush19 <72662629+krush19@users.noreply.github.com>
2022-02-13 20:01:56 +0530

    Update 09Feb22_Assignment.sql

--- week-2/09Feb22_Assignment.sql
+++ week-2/09Feb22_Assignment.sql
@@ -78,12 +78,122 @@ DROP SEQUENCE product_id;
 
 --###########################################################################################
 --(C). More on Groupings
-    --a. Grouping sets – introduce you to the grouping set concepts and show you how to generate multiple grouping sets in a query.
-    --b. CUBE – learn how to use CUBE to generate subtotals for all possible combinations of a specified group of dimensions.
-    --c. ROLLUP – describe how to calculate multiple levels of subtotals across a specified group of dimensions.
-    --d. PIVOT – show you how to transpose rows to columns to make the crosstab reports.
-    --e. UNPIVOT – a guide to rotating columns into rows.
+    --a. Grouping sets â€“ introduce you to the grouping set concepts and show you how to generate multiple grouping sets in a query.
+    --b. CUBE â€“ learn how to use CUBE to generate subtotals for all possible combinations of a specified group of dimensions.
+    --c. ROLLUP â€“ describe how to calculate multiple levels of subtotals across a specified group of dimensions.
+    --d. PIVOT â€“ show you how to transpose rows to columns to make the crosstab reports.
+    --e. UNPIVOT â€“ a guide to rotating columns into rows.
 --###########################################################################################
+--a. Grouping sets-
+--###########################################################################################
+SELECT 
+    deptno,job,SUM(sal)
+FROM 
+        emp
+GROUP BY 
+    deptno,job
+    
+UNION ALL
+
+SELECT 
+    deptno,NULL,SUM(sal)
+FROM 
+        emp
+GROUP BY 
+    deptno
+    
+UNION ALL 
+
+SELECT 
+    NULL,job,SUM(sal)
+FROM 
+        emp
+GROUP BY 
+    job
+    
+UNION ALL
+
+SELECT 
+    NULL,NULL,SUM(sal)
+FROM 
+        emp;
+
+-- instead of using union all we can use grouping sets and get same result .
+
+SELECT 
+    deptno,job,SUM(sal) AS total
+FROM
+    emp
+GROUP BY
+    GROUPING SETS
+        (   
+            ( deptno,job),
+            (deptno),
+            (job),
+            ()
+        );
+--################################################################################
+--b. CUBE â€“Uuse CUBE to generate subtotals for all possible combinations of a specified group of dimensions.
+    --The CUBE is an extension of the GROUP BY clause that allows you to generate grouping sets for
+    --all possible combinations of dimensions.
+--################################################################################
+SELECT 
+    deptno,job,SUM(sal),AVG(sal),MAX(sal),MIN(sal)
+FROM 
+    emp
+GROUP BY
+    CUBE(deptno,job);
+--cube is used when operation performed on two or more columns .
+--While rollup use when operation performed on only one column.
+--###########################################################################################
+--c. Rollup
+--###########################################################################################
+SELECT 
+    deptno,SUM(sal),AVG(sal),MAX(sal),MIN(sal)
+FROM 
+    emp
+GROUP BY
+    ROLLUP(deptno);
+
+--###########################################################################################
+--d.  PIVOT â€“ show you how to transpose rows to columns to make the crosstab reports.
+        --convert rows into columns.
+--###########################################################################################
+SELECT 
+    *
+FROM
+        (SELECT
+            job,deptno,sal
+        FROM
+            emp)
+        PIVOT (SUM(sal),count (*) count
+        FOR job IN ('CLERK','SALESMAN','ANALYST','MANAGER','PRESIDENT'));
+        
+SELECT 
+    *
+FROM
+        (SELECT
+            job,deptno,sal
+        FROM
+            emp)
+        PIVOT (SUM(sal),count (*) count
+        FOR deptno IN (10,20,30));
+
+--###########################################################################################
+--e.  UNPIVOT â€“ a guide to rotating columns into rows.
+              --convert columns into rows.
+--###########################################################################################
+SELECT 
+    *
+FROM
+        (SELECT
+          to_char(deptno) deptno,ename,job
+        FROM
+            emp)
+        UNPIVOT ( col_val FOR col_name IN (deptno,job,ename) );
+
+----------------------------------------------------------------------xxxxxxxxxxxxxxxxxxxx-------------------------------------xxxxxxxxxxxxxxxxxxxxxxx-----------------------------------------------------------------------------------    
+
 
 
 
